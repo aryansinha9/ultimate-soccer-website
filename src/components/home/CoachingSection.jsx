03dd90d28ae61a2ed5cadd35e7ec7a8d400b094e
Aryan Sinha <z5589770@ad.unsw.edu.au>
2025-09-22 14:47:30 +1000

    C to c

--- src/components/home/CoachingSection.jsx
+++ src/components/home/CoachingSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '@/Components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Link } from 'react-router-dom';
 
 // This is a temporary utility function. We will create a proper utils file later.
